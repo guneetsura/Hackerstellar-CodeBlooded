5affea95a925c09c9e7947c0ea486b0db904242b
Mayank Milind Kulkarni <85782107+mayankmk03@users.noreply.github.com>
2023-04-16 09:18:27 +0530

    Added API Code

---
+++ SmartContract.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const sdk = require('api')('@verbwire/v1.0#4psk2mplfwliyql');
+
+sdk.auth('sk_live_c71bc37b-6f12-4afb-9c31-ff2c3e5054bc');
+sdk.postNftDeployDeploysimplecontract({
+  chain: 'goerli',
+  contractType: 'nft721',
+  contractName: 'mayank',
+  contractSymbol: 'mayank123'
+}, {accept: 'application/json'})
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+
+  const parseCookies = (cookie = '') => 
+      cookie
+          .split(';')
+          .map(v => v.split('='))
+          .map(([k, ...vs]) => [k, vs.join('=')])
+          .reduce((acc, [k, v]) => {                            
+              acc[k.trim()] = decodeURIComponent(v);
+              return acc;
+          }, {});
+  
+  http.createServer((req, res) => {
+      const cookies = parseCookies(req.headers.cookie);
+      console.log(req.url, cookies);
+      res.writeHead(200, {'Set-Cookie': 'mycookie=643b02853af4bd2b173fd6bb'});
+      res.end('Hello Cookie');
+  })
+      .listen(8082, () => {
+          console.log('listening on port 8082')
+      })
